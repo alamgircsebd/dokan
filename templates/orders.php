f6fb46f62ccd92b2b36c687660dfa4ac3ebc47d5
Tareq Hasan <tareq1988@gmail.com>
2015-01-18 22:51:08 +0600

    translation strings in order listing page

--- templates/orders.php
+++ templates/orders.php
@@ -21,15 +21,15 @@
                 <form action="" method="GET" class="dokan-left">
                     <div class="dokan-form-group">
                         <label for="from">Date:</label> <input type="text" class="datepicker" name="order_date" id="order_date_filter" value="<?php echo isset( $_GET['order_date'] ) ? sanitize_key( $_GET['order_date'] ) : ''; ?>">
-                        <input type="submit" name="dokan_order_filter" class="dokan-btn dokan-theme" value="Filter">
+                        <input type="submit" name="dokan_order_filter" class="dokan-btn dokan-theme" value="<?php esc_attr_e( 'Filter', 'dokan' ); ?>">
                         <input type="hidden" name="order_status" value="<?php echo isset( $_GET['order_status'] ) ? sanitize_key( $_GET['order_status'] ) : 'all'; ?>">
                     </div>
                 </form>
 
                 <form action="" method="POST" class="dokan-right">
                     <div class="dokan-form-group">
-                        <input type="submit" name="dokan_order_export_all"  class="dokan-btn dokan-theme" value="Export All">
-                        <input type="submit" name="dokan_order_export_filtered"  class="dokan-btn dokan-theme" value="Export Filtered">
+                        <input type="submit" name="dokan_order_export_all"  class="dokan-btn dokan-theme" value="<?php esc_attr_e( 'Export All', 'dokan' ); ?>">
+                        <input type="submit" name="dokan_order_export_filtered"  class="dokan-btn dokan-theme" value="<?php esc_attr_e( 'Export Filtered', 'dokan' ); ?>">
                         <input type="hidden" name="order_date" value="<?php echo isset( $_GET['order_date'] ) ? sanitize_key( $_GET['order_date'] ) : ''; ?>">
                         <input type="hidden" name="order_status" value="<?php echo isset( $_GET['order_status'] ) ? sanitize_key( $_GET['order_status'] ) : 'all'; ?>">
                     </div>
