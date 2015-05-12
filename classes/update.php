0303a9b32a38b42a5223bd8eb6315aa814889b5b
Tareq Hasan <tareq1988@gmail.com>
2015-05-12 16:47:06 +0600

    Activation: Display verbose message on error. Base URL changed to https

--- classes/update.php
+++ classes/update.php
@@ -8,7 +8,7 @@
  */
 class Dokan_Update {
 
-    const base_url     = 'http://wedevs.com/';
+    const base_url     = 'https://wedevs.com/';
     const update_check = 'http://api.wedevs.com/update_check';
     const product_id   = 'dokan';
     const option       = 'dokan_license';
@@ -35,7 +35,7 @@ class Dokan_Update {
 
         add_filter( 'pre_set_site_transient_update_plugins', array($this, 'check_update') );
         add_filter( 'pre_set_transient_update_plugins', array($this, 'check_update') );
-        add_filter( 'plugins_api', array(&$this, 'check_info'), 10, 3 );
+        add_filter( 'plugins_api', array($this, 'check_info'), 10, 3 );
     }
 
     /**
@@ -148,7 +148,11 @@ class Dokan_Update {
 
         if ( is_wp_error( $response ) || $response['response']['code'] != 200 ) {
             if ( is_wp_error( $response ) ) {
-                echo '<div class="error"><p>Error: ' . $response->get_error_message() . '</p></div>';
+                echo '<div class="error"><p><strong>Dokan Activation Error:</strong> ' . $response->get_error_message() . '</p></div>';
+            }
+
+            if ( $response['response']['code'] != 200 ) {
+                echo '<div class="error"><p><strong>Dokan Activation Error:</strong> ' . $response['response']['code'] .' - ' . $response['response']['message'] . '</p></div>';
             }
 
             return false;
