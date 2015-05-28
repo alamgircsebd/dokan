6ebcb086eac52ae883641c563f106a8ae8739b8f
Tareq Hasan <tareq1988@gmail.com>
2015-05-28 17:45:46 +0600

    Fix spacing and docblock issues

--- classes/store-seo.php
+++ classes/store-seo.php
@@ -1,10 +1,11 @@
 <?php
-
 /**
  * Dokan SEO class
- * Integrates Dokan SEO template in front-end Settings menu
- *  
- * @author WeDevs
+ *
+ * Integrates Dokan SEO template in front-end Settings menu and provides
+ * SEO settings for single stores
+ *
+ * @since 2.3
  */
 class Dokan_Store_Seo {
 
@@ -26,22 +27,22 @@ class Dokan_Store_Seo {
         return $instance;
     }
 
-    /*
+    /**
      * Init hooks and filters
-     * 
+     *
+     * @return void
      */
-
     function init_hooks() {
 
         add_action( 'wp_ajax_dokan_seo_form_handler', array( $this, 'dokan_seo_form_handler' ) );
         add_action( 'template_redirect', array( $this, 'output_meta_tags' ) );
     }
 
-    /*
+    /**
      * Adds proper hooks for output of meta tags
-     * 
+     *
+     * @return void
      */
-
     function output_meta_tags() {
         if ( !dokan_is_store_page() ) {
             return;
@@ -84,11 +85,11 @@ class Dokan_Store_Seo {
 
     /*
      * prints out default meta tags from user meta
-     * 
+     *
      * @since 1.0.0
      * @param none
-     * 
-     * @return void            
+     *
+     * @return void
      */
 
     function print_tags() {
@@ -112,17 +113,18 @@ class Dokan_Store_Seo {
         }
     }
 
-    /* prints out social tags
-     * 
-     * @since 1.0.0     
+    /**
+     * Prints out social tags
+     *
+     * @since 1.0.0
      */
-
     function print_social_tags() {
         $meta_values = $this->store_info;
 
         if ( !isset( $meta_values['store_seo'] ) || $meta_values == false ) {
             return;
         }
+
         $og_title      = $meta_values['store_seo']['dokan-seo-og-title'];
         $og_desc       = $meta_values['store_seo']['dokan-seo-og-desc'];
         $og_img        = $meta_values['store_seo']['dokan-seo-og-image'];
@@ -133,33 +135,38 @@ class Dokan_Store_Seo {
         if ( $og_title ) {
             echo PHP_EOL . '<meta property="og:title" content="' . $this->print_saved_meta( $og_title ) . '"/>';
         }
+
         if ( $og_desc ) {
             echo PHP_EOL . '<meta property="og:description" content="' . $this->print_saved_meta( $og_desc ) . '"/>';
         }
+
         if ( $og_img ) {
             echo PHP_EOL . '<meta property="og:image" content="' . wp_get_attachment_url( $og_img ) . '"/>';
         }
+
         if ( $twitter_title ) {
             echo PHP_EOL . '<meta name="twitter:title" content="' . $this->print_saved_meta( $twitter_title ) . '"/>';
         }
+
         if ( $twitter_desc ) {
             echo PHP_EOL . '<meta name="twitter:description" content="' . $this->print_saved_meta( $twitter_desc ) . '"/>';
         }
+
         if ( $twitter_img ) {
             echo PHP_EOL . '<meta name="twitter:image" content="' . wp_get_attachment_url( $twitter_img ) . '"/>';
         }
     }
 
-    /* Generic meta replacer for meta tags
-     * 
+    /**
+     * Generic meta replacer for meta tags
+     *
      * @since 1.0.0
+     *
      * @param string val, string meta_name, string meta_type
+     *
      * @return string meta
-     * 
      */
-
     function replace_meta( $val_default, $meta, $type = '' ) {
-
 
         $meta_values = $this->store_info;
 
@@ -172,69 +179,72 @@ class Dokan_Store_Seo {
 
         if ( $val ) {
             return $val;
-        } else {
-            return $val_default;
         }
+
+        return $val_default;
     }
 
-    /*
-     * Replace title meta of other SEO plugin 
-     * 
+    /**
+     * Replace title meta of other SEO plugin
+     *
      * @param title
      * @since 1.0.0
-     * 
+     *
      * @return string title
      */
-
     function replace_title( $title ) {
         return $this->replace_meta( $title, 'title', 'meta' );
     }
 
-    /*
-     * Replace keywords meta of other SEO plugin 
-     * 
+    /**
+     * Replace keywords meta of other SEO plugin
+     *
      * @param keywords
+     *
      * @since 1.0.0
-     * 
+     *
      * @return keywords
      */
-
     function replace_keywords( $keywords ) {
         return $this->replace_meta( $keywords, 'keywords', 'meta' );
     }
 
-    /*
-     * Replace description meta of other SEO plugin 
+    /**
+     * Replace description meta of other SEO plugin
+     *
      * @param desc
+     *
      * @since 1.0.0
-     * 
+     *
      * @return desc
      */
-
     function replace_desc( $desc ) {
         return $this->replace_meta( $desc, 'desc', 'meta' );
     }
 
-    /* replace OG tag title for WP_SEO
+    /**
+     * Replace OG tag title for WP_SEO
+     *
      * @since 1.0.0
      */
-
     function replace_og_title( $title ) {
         return $this->replace_meta( $title, 'title', 'og' );
     }
 
-    /* replace OG tag description for WP_SEO
+    /**
+     * Replace OG tag description for WP_SEO
+     *
      * @since 1.0.0
      */
-
     function replace_og_desc( $desc ) {
         return $this->replace_meta( $desc, 'desc', 'og' );
     }
 
-    /* replace OG tag Image for WP_SEO
+    /**
+     * Replace OG tag Image for WP_SEO
+     *
      * @since 1.0.0
      */
-
     function replace_og_img( $img ) {
         $img_default = $img;
 
@@ -266,29 +276,35 @@ class Dokan_Store_Seo {
         }
     }
 
-    /* replace twitter tag title for WP_SEO
-     * 
-     * 
+    /**
+     * Replace twitter tag title for WP_SEO
+     *
+     * @param  string
+     *
+     * @return string
      */
-
     function replace_twitter_title( $val_default ) {
         return $this->replace_meta( $val_default, 'title', 'twitter' );
     }
 
-    /* replace twitter tag description for WP_SEO
-     * 
-     * 
+    /**
+     * replace twitter tag description for WP_SEO
+     *
+     * @param  string
+     *
+     * @return string
      */
-
     function replace_twitter_desc( $val_default ) {
         return $this->replace_meta( $val_default, 'desc', 'twitter' );
     }
 
-    /* replace twitter image tag for WP_SEO
-     * 
-     * 
+    /**
+     * Replace twitter image tag for WP_SEO
+     *
+     * @param  string
+     *
+     * @return string
      */
-
     function replace_twitter_img( $img ) {
 
         $img_default = $img;
@@ -301,16 +317,18 @@ class Dokan_Store_Seo {
 
         $img = $meta_values['store_seo']['dokan-seo-twitter-image'];
 
-        if ( $img )
+        if ( $img ) {
             return wp_get_attachment_url( $img );
-        else
-            return $img_default;
+        }
+
+        return $img_default;
     }
 
-    /*
-     * prints out twitter image tag
+    /**
+     * Prints out twitter image tag
+     *
+     * @return void
      */
-
     function print_twitter_img() {
         $meta_values = $this->store_info;
 
@@ -325,13 +343,11 @@ class Dokan_Store_Seo {
         }
     }
 
-    /*
-     * print SEO meta input form on frontend
-     * 
-     * @since 1.0.0
-     * 
+    /**
+     * Print SEO meta input form on frontend
+     *
+     * @return void
      */
-
     function frontend_meta_form() {
         $current_user   = get_current_user_id();
         $seller_profile = dokan_get_store_info( $current_user );
@@ -350,7 +366,7 @@ class Dokan_Store_Seo {
         );
 
         $seo_meta = wp_parse_args( $seo_meta, $default_store_seo );
-        ?>  
+        ?>
         <div class="dokan-alert dokan-hide" id="dokan-seo-feedback"></div>
         <form method="post" id="dokan-store-seo-form"  action="" class="dokan-form-horizontal">
 
@@ -359,10 +375,10 @@ class Dokan_Store_Seo {
                     <span class="dokan-tooltips-help tips" title="" data-original-title="<?php _e( 'SEO Title is shown as the title of your store page', 'dokan' ); ?>">
                         <i class="fa fa-question-circle"></i>
                     </span>
-                </label>                
+                </label>
                 <div class="dokan-w5 dokan-text-left">
                     <input id="dokan-seo-meta-title" value="<?php echo $this->print_saved_meta( $seo_meta['dokan-seo-meta-title'] ) ?>" name="dokan-seo-meta-title" placeholder=" " class="dokan-form-control input-md" type="text">
-                </div>                         
+                </div>
             </div>
 
             <div class="dokan-form-group">
@@ -384,7 +400,7 @@ class Dokan_Store_Seo {
                 </label>
                 <div class="dokan-w7 dokan-text-left">
                     <input id="dokan-seo-meta-keywords" value="<?php echo $this->print_saved_meta( $seo_meta['dokan-seo-meta-keywords'] ) ?>" name="dokan-seo-meta-keywords" placeholder=" " class="dokan-form-control input-md" type="text">
-                </div>                         
+                </div>
             </div>
 
             <?php $this->print_fb_meta_form( $seo_meta ); ?>
@@ -392,20 +408,20 @@ class Dokan_Store_Seo {
 
             <?php wp_nonce_field( 'dokan_store_seo_form_action', 'dokan_store_seo_form_nonce' ); ?>
 
-            <div class="dokan-form-group" style="margin-left: 23%">   
+            <div class="dokan-form-group" style="margin-left: 23%">
                 <input type="submit" id='dokan-store-seo-form-submit' class="dokan-left dokan-btn dokan-btn-theme" value="<?php esc_attr_e( 'Save Changes', 'dokan' ); ?>">
             </div>
         </form>
         <?php
     }
 
-    /*
+    /**
      * print social meta input fields
-     * 
-     * @since 1.0.0
-     * 
+     *
+     * @param  array  $seo_meta
+     *
+     * @return void
      */
-
     function print_fb_meta_form( $seo_meta ) {
         ?>
 
@@ -413,14 +429,14 @@ class Dokan_Store_Seo {
             <label class="dokan-w3 dokan-control-label" for="dokan-seo-og-title"><?php _e( 'Facebook Title :', 'dokan' ); ?></label>
             <div class="dokan-w5 dokan-text-left">
                 <input id="dokan-seo-og-title" value="<?php echo $this->print_saved_meta( $seo_meta['dokan-seo-og-title'] ) ?>" name="dokan-seo-og-title" placeholder=" " class="dokan-form-control input-md" type="text">
-            </div>                         
+            </div>
         </div>
 
         <div class="dokan-form-group">
             <label class="dokan-w3 dokan-control-label" for="dokan-seo-og-desc"><?php _e( 'Facebook Description :', 'dokan' ); ?></label>
             <div class="dokan-w5 dokan-text-left">
                 <textarea class="dokan-form-control" rows="3" id="dokan-seo-og-desc" name="dokan-seo-og-desc"><?php echo $this->print_saved_meta( $seo_meta['dokan-seo-og-desc'] ) ?></textarea>
-            </div>               
+            </div>
         </div>
         <?php
         $og_image     = $seo_meta['dokan-seo-og-image'] ? $seo_meta['dokan-seo-og-image'] : 0;
@@ -429,7 +445,7 @@ class Dokan_Store_Seo {
         <div class="dokan-form-group ">
             <label class="dokan-w3 dokan-control-label" for="dokan-seo-og-image"><?php _e( 'Facebook Image :', 'dokan' ); ?></label>
             <div class="dokan-w5 dokan-gravatar dokan-seo-image">
-                <div class="dokan-left gravatar-wrap<?php echo $og_image ? '' : ' dokan-hide'; ?>">                    
+                <div class="dokan-left gravatar-wrap<?php echo $og_image ? '' : ' dokan-hide'; ?>">
                     <input type="hidden" class="dokan-file-field" value="<?php echo $og_image; ?>" name="dokan-seo-og-image">
                     <img class="dokan-gravatar-img" src="<?php echo esc_url( $og_image_url ); ?>">
                     <a class="dokan-close dokan-remove-gravatar-image">&times;</a>
@@ -440,18 +456,16 @@ class Dokan_Store_Seo {
                 </div>
             </div>
         </div>
-
-
-
-
     <?php
     }
 
-    /*
-     * print twitter meta form
-     * @since 1.0.0
+    /**
+     * Print twitter meta form
+     *
+     * @param  array  $seo_meta
+     *
+     * @return void
      */
-
     function print_twitter_meta_form( $seo_meta ) {
         ?>
 
@@ -459,14 +473,14 @@ class Dokan_Store_Seo {
             <label class="dokan-w3 dokan-control-label" for="dokan-seo-twitter-title"><?php _e( 'Twitter Title :', 'dokan' ); ?></label>
             <div class="dokan-w5 dokan-text-left">
                 <input id="dokan-seo-twitter-title" value="<?php echo $this->print_saved_meta( $seo_meta['dokan-seo-twitter-title'] ) ?>" name="dokan-seo-twitter-title" placeholder=" " class="dokan-form-control input-md" type="text">
-            </div>                         
+            </div>
         </div>
 
         <div class="dokan-form-group">
             <label class="dokan-w3 dokan-control-label" for="dokan-seo-twitter-desc"><?php _e( 'Twitter Description :', 'dokan' ); ?></label>
             <div class="dokan-w5 dokan-text-left">
                 <textarea class="dokan-form-control" rows="3" id="dokan-seo-twitter-desc" name="dokan-seo-twitter-desc"><?php echo $this->print_saved_meta( $seo_meta['dokan-seo-twitter-desc'] ) ?></textarea>
-            </div>               
+            </div>
         </div>
         <?php
         $twitter_image     = $seo_meta['dokan-seo-twitter-image'] ? $seo_meta['dokan-seo-twitter-image'] : 0;
@@ -475,7 +489,7 @@ class Dokan_Store_Seo {
         <div class="dokan-form-group ">
             <label class="dokan-w3 dokan-control-label" for="dokan-seo-twitter-image"><?php _e( 'Twitter Image :', 'dokan' ); ?></label>
             <div class="dokan-w5 dokan-gravatar dokan-seo-image">
-                <div class="dokan-left gravatar-wrap<?php echo $twitter_image ? '' : ' dokan-hide'; ?>">                    
+                <div class="dokan-left gravatar-wrap<?php echo $twitter_image ? '' : ' dokan-hide'; ?>">
                     <input type="hidden" class="dokan-file-field" value="<?php echo $twitter_image; ?>" name="dokan-seo-twitter-image">
                     <img class="dokan-gravatar-img" src="<?php echo esc_url( $twitter_image_url ); ?>">
                     <a class="dokan-close dokan-remove-gravatar-image">&times;</a>
@@ -490,13 +504,13 @@ class Dokan_Store_Seo {
         <?php
     }
 
-    /* check meta data and print
-     * @since 1.0.0
-     * @param bool || string $val
-     * 
-     * @return string $val
+    /**
+     * Check meta data and print
+     *
+     * @param  boolean|string  $val
+     *
+     * @return string
      */
-
     function print_saved_meta( $val ) {
         if ( $val == false )
             return '';
@@ -504,6 +518,11 @@ class Dokan_Store_Seo {
             return esc_attr( $val );
     }
 
+    /**
+     * Submit handler for settings form
+     *
+     * @return void
+     */
     function dokan_seo_form_handler() {
         parse_str( $_POST['data'], $postdata );
 
@@ -535,10 +554,9 @@ class Dokan_Store_Seo {
 
         update_user_meta( $current_user, 'dokan_profile_settings', $seller_profile );
 
-        wp_send_json_success( 'Your Changes Have been Updated' );
+        wp_send_json_success( __( 'Your changes has been updated!', 'dokan' ) );
     }
 
 }
 
 $seo = Dokan_Store_Seo::init();
-
