ab635c5372d80d501978e6921b5f8f4e71d1bdbb
SK Shaikat <sk.shaikat18@gmail.com>
2015-06-04 18:23:40 +0600

    fix removed shipping calculator error from ajax.php and less

--- classes/ajax.php
+++ classes/ajax.php
@@ -71,6 +71,14 @@ class Dokan_Ajax {
         add_action( 'wp_ajax_dokan_announcement_remove_row', array( $this, 'remove_announcement') );
         add_action( 'wp_ajax_nopriv_dokan_announcement_remove_row', array( $this, 'remove_announcement') );
 
+        // shipping state ajax
+        add_action( 'wp_ajax_nopriv_dokan_shipping_country_select', array( $this, 'get_state_by_shipping_country') );
+        add_action( 'wp_ajax_dokan_shipping_country_select', array( $this, 'get_state_by_shipping_country') );
+
+        // shipping calculation ajax
+        add_action( 'wp_ajax_nopriv_dokan_shipping_calculator', array( $this, 'get_calculated_shipping_cost') );
+        add_action( 'wp_ajax_dokan_shipping_calculator', array( $this, 'get_calculated_shipping_cost') );
+
         // Single product Design ajax
         add_action( 'wp_ajax_dokan_save_attributes_options', array( $this, 'save_attributes_options') );
         add_action( 'wp_ajax_nopriv_dokan_save_attributes_options', array( $this, 'save_attributes_options') );
@@ -1218,5 +1226,95 @@ class Dokan_Ajax {
         } else {
             wp_send_json_error();
         }
+    }
+
+
+    /**    
+     * get state by shipping country
+     *
+     * @return
+     */
+    function get_state_by_shipping_country() {
+        global $post;
+        $dps_state_rates   = get_user_meta( $_POST['author_id'], '_dps_state_rates', true );
+        $country_obj = new WC_Countries();
+        $states      = $country_obj->states;
+
+        $country = $_POST['country_id'];
+        ob_start(); ?>
+        <?php 
+        if ( isset( $dps_state_rates[$country] ) && count( $dps_state_rates[$country] ) ) { ?>
+            <label for="dokan-shipping-state" class="dokan-control-label"><?php _e( 'State', 'dokan' ); ?></label>
+            <select name="dokan-shipping-state" class="dokan-shipping-state dokan-form-control" id="dokan-shipping-state">
+                <option value=""><?php _e( '--Select State--', 'dokan' ); ?></option>
+                <?php foreach ($dps_state_rates[$country] as $state_code => $state_cost ): ?>
+                    <option value="<?php echo $state_code ?>"><?php
+                        if ( $state_code == 'everywhere' ) {
+                            _e( 'Other States', 'dokan' );
+                        } else {
+                            if( isset( $states[$country][$state_code] ) ) {
+                                echo $states[$country][$state_code];
+                            } else {
+                                echo $state_code;
+                            }
+                        }
+                    ?></option>
+                <?php endforeach ?>
+            </select>
+        <?php 
+        }
+        $content = ob_get_clean();
+        
+        wp_send_json_success( $content );
+    }
+
+    /**
+     * calculate shipping rate in single product page
+     *
+     * @return
+     */
+    function get_calculated_shipping_cost() {
+        global $post;
+        
+        $dps_country_rates = get_user_meta( $_POST['author_id'], '_dps_country_rates', true );
+        $dps_state_rates   = get_user_meta( $_POST['author_id'], '_dps_state_rates', true );
+        // Store wide shipping info
+        $store_shipping_type_price    = (float)get_user_meta( $_POST['author_id'], '_dps_shipping_type_price', true );
+        $additional_product_cost      = (float)get_post_meta( $_POST['product_id'], '_additional_price', true );
+        $base_shipping_type_price     = ( (float)$store_shipping_type_price + ( ($additional_product_cost) ? (float)$additional_product_cost : 0 ) );
+        $additional_qty_product_price = get_post_meta( $_POST['product_id'], '_additional_qty', true );
+        $dps_additional_qty           = get_user_meta( $_POST['author_id'], '_dps_additional_qty', true );
+        $additional_qty_price         = ( $additional_qty_product_price ) ? $additional_qty_product_price : $dps_additional_qty;
+
+        if ( isset( $_POST['country_id'] ) || !empty( $_POST['country_id'] ) ) {
+            $country = $_POST['country_id'];
+        } else {
+            $country = '';
+        }
+        if ( isset( $_POST['quantity'] ) && $_POST['quantity'] > 0 ) {
+            $quantity = $_POST['quantity'];
+        } else {
+            $quantity = 1;
+        }
+        $additional_quantity_cost = ( $quantity - 1 ) * $additional_qty_price;
+        $flag = '';
+        ob_start(); ?>
+
+        <?php 
+        if ( $country != '' ) {
+            if ( isset( $dps_state_rates[$country] ) && count( $dps_state_rates[$country] ) && empty( $_POST['state'] ) ) {
+                _e( 'Please select a State from the dropdown', 'dokan' );
+            } else if ( !isset( $dps_state_rates[$country] ) && empty( $_POST['state'] ) ) {
+                echo __( 'Shipping Cost : ', 'dokan' ) . '<h4>' . wc_price( $dps_country_rates[$country] + $base_shipping_type_price + $additional_quantity_cost ) . '</h4>';
+            } else if ( isset( $_POST['state'] ) && !empty( $_POST['state'] ) ) {
+                $state = $_POST['state'];
+                echo __( 'Shipping Cost : ', 'dokan' ) . '<h4>' . wc_price( $dps_state_rates[$country][$state] + $base_shipping_type_price + $additional_quantity_cost ) . '</h4>';
+            }
+        } else {
+            _e( 'Please select a country from the dropdown', 'dokan' );
+        }
+        $content = ob_get_clean();
+        
+        wp_send_json_success( $content );
     }
 }
