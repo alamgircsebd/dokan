aff5e4dcc7f058c78f227b6aed4cd41a8d49303e
Tareq Hasan <tareq1988@gmail.com>
2016-01-30 16:39:26 +0600

    pre-populate store contact form fields

--- includes/pro/templates/widgets/store-contact-form.php
+++ includes/pro/templates/widgets/store-contact-form.php
@@ -6,16 +6,24 @@
  *
  * @package dokan
  */
+
+$name = $email = '';
+
+if ( is_user_logged_in() ) {
+    $user  = wp_get_current_user();
+    $name  = $user->display_name;
+    $email = $user->user_email;
+}
 ?>
 
 <form id="dokan-form-contact-seller" action="" method="post" class="seller-form clearfix">
     <div class="ajax-response"></div>
     <ul>
         <li class="dokan-form-group">
-            <input type="text" name="name" value="" placeholder="<?php esc_attr_e( 'Your Name', 'dokan' ); ?>" class="dokan-form-control" minlength="5" required="required">
+            <input type="text" name="name" value="<?php echo esc_attr( $name ); ?>" placeholder="<?php esc_attr_e( 'Your Name', 'dokan' ); ?>" class="dokan-form-control" minlength="5" required="required">
         </li>
         <li class="dokan-form-group">
-            <input type="email" name="email" value="" placeholder="<?php esc_attr_e( 'you@example.com', 'dokan' ); ?>" class="dokan-form-control" required="required">
+            <input type="email" name="email" value="<?php echo esc_attr( $email ); ?>" placeholder="<?php esc_attr_e( 'you@example.com', 'dokan' ); ?>" class="dokan-form-control" required="required">
         </li>
         <li class="dokan-form-group">
             <textarea  name="message" maxlength="1000" cols="25" rows="6" value="" placeholder="<?php esc_attr_e( 'Type your messsage...', 'dokan' ); ?>" class="dokan-form-control" required="required"></textarea>
