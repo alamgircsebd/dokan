33a42ebfce6507bc3ff44d99f297d58da4a47c8f
Sk Shaikat <sk.shaikat18@gmail.com>
2016-03-06 16:00:45 +0600

    Fix undefine error on cart page for shipping additional price

--- includes/pro/includes/shipping-gateway/shipping.php
+++ includes/pro/includes/shipping-gateway/shipping.php
@@ -267,7 +267,7 @@ class Dokan_WC_Shipping extends WC_Shipping_Method {
 
         if ( !empty( $price ) ) {
             foreach ( $price as $s_id => $value ) {
-                $amount = $amount + ( array_sum( $value['addition_price'] )+$value['default']+array_sum( $value['qty'] )+$value['add_product']+ ( isset($value['state_rates']) ? $value['state_rates'] : 0 ) );
+                $amount = $amount + ( array_sum( isset( $value['addition_price'] ) ? $value['addition_price'] : 0 )+$value['default']+array_sum( $value['qty'] )+$value['add_product']+ ( isset($value['state_rates']) ? $value['state_rates'] : 0 ) );
             }
         }
 
