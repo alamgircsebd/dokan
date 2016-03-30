896736bee66f6c7f44992f7f0699eff3c8bb7255
Sk Shaikat <sk.shaikat18@gmail.com>
2016-03-30 12:12:21 +0600

    add new update on seller role capabilities

---
+++ includes/upgrades/dokan-upgrade-2.4.11.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Upgrade capabilities for sellers
+ *
+ * @since 2.4.11
+ *
+ * @return void
+ */
+function dokan_upgrade_seller_cap_2411() {
+	global $wp_roles;
+	$wp_roles->add_cap( 'seller', 'edit_shop_orders' );
+}
+
+dokan_upgrade_seller_cap_2411();
