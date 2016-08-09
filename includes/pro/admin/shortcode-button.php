e2f0b8b8b847359eb4e7c2a94f15a0a6394de4a6
rafsuntaskin <rafsun.aiub@gmail.com>
2016-08-09 13:23:45 +0600

    added more shortcodes

--- includes/pro/admin/shortcode-button.php
+++ includes/pro/admin/shortcode-button.php
@@ -1,8 +1,15 @@
 <?php
 
-
+/**
+ * Dokan tinyMce Shortcode Button class 
+ * 
+ * @since 2.4.12
+ */
 class Dokan_shortcodes_button {
     
+    /**
+     * Constructor for shortcode class
+     */
     public function __construct() {
         
         add_filter( 'mce_external_plugins',  array( $this, 'enqueue_plugin_scripts' ) );
@@ -11,17 +18,35 @@ class Dokan_shortcodes_button {
         add_action( 'admin_enqueue_scripts', array( $this, 'localize_shortcodes' ) , 90  );
     }
     
+    /**
+     * Generate shortcode array
+     * 
+     * @since 2.4.12
+     * 
+     */
     function localize_shortcodes() {
         
-        $shortcodes = array( 
+        $shortcodes = array(
             array(
-                'title' => 'Dokan Dashboard',
+                'title'   => 'Dokan Dashboard',
                 'content' => '[dokan-dashboard]'
             ),
             array(
-                'title' => 'Store Listing',
+                'title'   => 'Store Listing',
                 'content' => '[dokan-stores]'
             ),
+            array(
+                'title'   => 'Best Selling Product',
+                'content' => '[dokan-best-selling-product]'
+            ),
+            array(
+                'title'   => 'Top Rated Product',
+                'content' => '[dokan-top-rated-product]'
+            ),
+            array(
+                'title'   => 'Dokan My Orders',
+                'content' => '[dokan-my-orders]'
+            )
         );
         wp_localize_script( 'dokan_slider_admin', 'dokan_shortcodes', apply_filters( 'dokan_button_shortcodes', $shortcodes ) );
     }
@@ -43,13 +68,30 @@ class Dokan_shortcodes_button {
         return $instance;
     }
 
-    
+    /**
+     * Add button on Post Editor
+     * 
+     * @since 2.4.12
+     * 
+     * @param array $plugin_array
+     * 
+     * @return array
+     */
     function enqueue_plugin_scripts( $plugin_array ) {
         //enqueue TinyMCE plugin script with its ID.
         $plugin_array["dokan_button"] =  DOKAN_PLUGIN_ASSEST . "/js/dokan-tmc-button.js";
         return $plugin_array;
     }
     
+    /**
+     * Register tinyMce button
+     * 
+     * @since 2.4.12
+     * 
+     * @param array $buttons
+     * 
+     * @return array
+     */
     function register_buttons_editor( $buttons ) {
         //register buttons with their id.
         array_push( $buttons, "dokan_button" );
