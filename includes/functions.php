d9c5e1324d2a92db5e4c20551da90783739502b0
Sk Shaikat <sk.shaikat18@gmail.com>
2016-08-10 12:50:13 +0600

    remove seller mail on parent order

--- includes/functions.php
+++ includes/functions.php
@@ -1474,6 +1474,10 @@ function dokan_get_processing_time_value( $index ) {
  */
 function dokan_wc_email_recipient_add_seller( $email, $order ) {
 
+    if ( get_post_meta( $order->id, 'has_sub_order', true ) == true ) {
+        return;
+    }
+    
     if ( $order ) {
 
         $sellers = dokan_get_seller_id_by_order( $order->id );
