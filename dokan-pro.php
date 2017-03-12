f3761516f5aacad2c0decf7aa3b783dbd51adf62
Sabbir Ahmed <sabbir.081070@gmail.com>
2017-03-12 09:26:42 +0600

    Change plugin dir path

--- dokan-pro.php
+++ dokan-pro.php
@@ -85,18 +85,18 @@ class Dokan_Pro {
             <h2><?php _e( 'Your Dokan Pro is almost ready!', 'dokan' ); ?></h2>
 
             <?php
-                $plugin_file = 'dokan-pro/dokan-pro.php';
+                $plugin_file = basename( dirname( __FILE__ ) ) . '/dokan-pro.php';
                 $core_plugin_file = 'dokan-lite/dokan.php';
             ?>
             <a href="<?php echo wp_nonce_url( 'plugins.php?action=deactivate&amp;plugin=' . $plugin_file . '&amp;plugin_status=all&amp;paged=1&amp;s=', 'deactivate-plugin_' . $plugin_file ); ?>" class="notice-dismiss" style="text-decoration: none;" title="<?php _e( 'Dismiss this notice', 'dokan' ); ?>"></a>
 
             <?php if ( file_exists( WP_PLUGIN_DIR . '/' . $core_plugin_file ) && is_plugin_inactive( 'dokan-lite' ) ): ?>
-                <p><?php _e( 'You just need to activate the Dokan lite plugin to make it functional.', 'dokan' ); ?></p>
+                <p><?php echo sprintf( __( 'You just need to activate the <strong>%s</strong> to make it functional.', 'dokan' ), 'Dokan (Lite) - Multi-vendor Marketplace plugin' ); ?></p>
                 <p>
                     <a class="button button-primary" href="<?php echo wp_nonce_url( 'plugins.php?action=activate&amp;plugin=' . $core_plugin_file . '&amp;plugin_status=all&amp;paged=1&amp;s=', 'activate-plugin_' . $core_plugin_file ); ?>"  title="<?php _e( 'Activate this plugin', 'dokan' ); ?>"><?php _e( 'Activate', 'dokan' ); ?></a>
                 </p>
             <?php else: ?>
-                <p><?php echo sprintf( __( "You just need to install the %sCore Plugin%s to make it functional.", "dokan-pro" ), '<a target="_blank" href="https://wordpress.org/plugins/dokan-lite/">', '</a>' ); ?></p>
+                <p><?php echo sprintf( __( "You just need to install the %sCore Plugin%s to make it functional.", "dokan" ), '<a target="_blank" href="https://wordpress.org/plugins/dokan-lite/">', '</a>' ); ?></p>
 
                 <p>
                     <button id="dokan-pro-installer" class="button"><?php _e( 'Install Now', 'dokan' ); ?></button>
