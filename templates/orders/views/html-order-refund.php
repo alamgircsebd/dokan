91d39dfcdba39db36f5c8a6207a04877b8d63b7d
Sabbir Ahmed <sabbir.081070@gmail.com>
2017-04-10 08:35:18 +0600

    Refund displaying issues

--- templates/orders/views/html-order-refund.php
+++ templates/orders/views/html-order-refund.php
@@ -6,35 +6,40 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * @var object $refund The refund object.
  */
-$who_refunded = new WP_User( $refund->post->post_author );
+
+// var_dump( $refund->get_id() );
+
+$refund_post = get_post( dokan_cmp_get_prop( $refund, 'id' ) );
+
+$who_refunded = new WP_User( $refund_post->post_author );
 ?>
-<tr class="refund <?php echo ( ! empty( $class ) ) ? $class : ''; ?>" data-order_refund_id="<?php echo $refund->id; ?>">
+<tr class="refund <?php echo ( ! empty( $class ) ) ? $class : ''; ?>" data-order_refund_id="<?php echo dokan_cmp_get_prop( $refund, 'id' ); ?>">
 	<!-- <td class="check-column"></td> -->
 
 	<td class="thumb"><div><i class="fa fa-money"></i></div></td>
 
 	<td class="name">
 		<?php
-			echo esc_attr__( 'Refund', 'dokan' ) . ' #' . absint( $refund->id ) . ' - ' . esc_attr( date_i18n( get_option( 'date_format' ) . ', ' . get_option( 'time_format' ), strtotime( $refund->post->post_date ) ) );
+			echo esc_attr__( 'Refund', 'dokan' ) . ' #' . absint( dokan_cmp_get_prop( $refund, 'id' ) ) . ' - ' . esc_attr( date_i18n( get_option( 'date_format' ) . ', ' . get_option( 'time_format' ), strtotime( $refund_post->post_date ) ) );
 
 			if ( $who_refunded->exists() ) {
 				echo ' ' . esc_attr_x( 'by', 'Ex: Refund - $date >by< $username', 'dokan' ) . ' ' . '<abbr class="refund_by" title="' . esc_attr__( 'ID: ', 'dokan' ) . absint( $who_refunded->ID ) . '">' . esc_attr( $who_refunded->display_name ) . '</abbr>' ;
 			}
 		?>
-		<?php if ( $refund->get_refund_reason() ) : ?>
-			<p class="description"><?php echo esc_html( $refund->get_refund_reason() ); ?></p>
+		<?php if ( dokan_cmp_get_prop( $refund, 'get_refund_reason', 'get_reason' ) ) : ?>
+			<p class="description"><?php echo esc_html(  dokan_cmp_get_prop( $refund, 'get_refund_reason', 'get_reason' ) ); ?></p>
 		<?php endif; ?>
-		<input type="hidden" class="order_refund_id" name="order_refund_id[]" value="<?php echo esc_attr( $refund->id ); ?>" />
+		<input type="hidden" class="order_refund_id" name="order_refund_id[]" value="<?php echo esc_attr( dokan_cmp_get_prop( $refund, 'id' ) ); ?>" />
 	</td>
 
-	<?php do_action( 'woocommerce_admin_order_item_values', null, $refund, absint( $refund->id ) ); ?>
+	<?php do_action( 'woocommerce_admin_order_item_values', null, $refund, absint( dokan_cmp_get_prop( $refund, 'id' ) ) ); ?>
 
 	<td class="item_cost" width="1%">&nbsp;</td>
 	<td class="quantity" width="1%">&nbsp;</td>
 
 	<td class="line_cost" width="1%">
 		<div class="view">
-			<?php echo wc_price( '-' . $refund->get_refund_amount() ); ?>
+			<?php echo wc_price( '-' .  dokan_cmp_replace_func( 'get_refund_amount', 'get_amount', $refund ) ); ?>
 		</div>
 	</td>
 
