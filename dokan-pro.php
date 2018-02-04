d3337c6179888ec40064fcddc3ac14a3696f9ee0
Sabbir Ahmed <sabbir.081070@gmail.com>
2018-02-04 11:57:03 +0600

    Version bump, changelog updated, Generate pot file

--- dokan-pro.php
+++ dokan-pro.php
@@ -3,9 +3,11 @@
   Plugin Name: Dokan Pro
   Plugin URI: https://wedevs.com/dokan/
   Description: An e-commerce marketplace plugin for WordPress. Powered by WooCommerce and weDevs.
-  Version: 2.7.2
+  Version: 2.7.3
   Author: weDevs
   Author URI: https://wedevs.com/
+  WC requires at least: 2.6
+  WC tested up to: 3.2.6
   License: GPL2
   TextDomain: dokan
  */
@@ -34,7 +36,7 @@ class Dokan_Pro {
      *
      * @var string
      */
-    public $version = '2.7.2';
+    public $version = '2.7.3';
 
     /**
      * Constructor for the Dokan_Pro class
