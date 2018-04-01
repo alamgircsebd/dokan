de0aed0766e37171eafca50dfd1f3a2d1e92f421
Mohammed Saimon <saimon2721@gmail.com>
2018-04-01 18:12:37 +0600

    added user commssion type

--- includes/modules/subscription/includes/admin/admin.php
+++ includes/modules/subscription/includes/admin/admin.php
@@ -652,8 +652,10 @@ class DPS_Admin {
             }
         }
 
-        $pack_validity = get_post_meta( $pack_id, '_pack_validity', true );
-        $admin_commission = get_post_meta( $pack_id, '_subscription_product_admin_commission', true );
+        $pack_validity           = get_post_meta( $pack_id, '_pack_validity', true );
+        $admin_commission        = get_post_meta( $pack_id, '_subscription_product_admin_commission', true );
+        $admin_commission_type   = get_post_meta( $pack_id, '_subscription_product_admin_commission_type', true );
+
         update_user_meta( $user_id, 'product_package_id', $pack_id );
         update_user_meta( $user_id, 'product_order_id', '' );
         update_user_meta( $user_id, 'product_no_with_pack' , get_post_meta( $pack_id, '_no_of_product', true ) ); //number of products
@@ -677,6 +679,7 @@ class DPS_Admin {
 
         if ( ! empty( $admin_commission ) ) {
             update_user_meta( $user_id, 'dokan_admin_percentage', $admin_commission );
+            update_post_meta( $user_id, 'dokan_admin_percentage_type', $admin_commission_type );
         } else {
             update_user_meta( $user_id, 'dokan_admin_percentage', '' );
         }
