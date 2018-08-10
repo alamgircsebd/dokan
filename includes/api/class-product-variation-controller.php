d4ce9b3aeeb05eb5b1ff995e2ba1fdc86c6b9d90
Sabbir Ahmed <sabbir.081070@gmail.com>
2018-08-10 10:29:23 +0600

    fix: variation api schema added

--- includes/api/class-product-variation-controller.php
+++ includes/api/class-product-variation-controller.php
@@ -454,5 +454,364 @@ class Dokan_REST_Product_Variation_Controller extends Dokan_REST_Product_Control
         return $links;
     }
 
+    /**
+     * Get the Variation's schema, conforming to JSON Schema.
+     *
+     * @return array
+     */
+    public function get_item_schema() {
+        $weight_unit    = get_option( 'woocommerce_weight_unit' );
+        $dimension_unit = get_option( 'woocommerce_dimension_unit' );
+        $schema         = array(
+            '$schema'    => 'http://json-schema.org/draft-04/schema#',
+            'title'      => $this->post_type,
+            'type'       => 'object',
+            'properties' => array(
+                'id'                    => array(
+                    'description' => __( 'Unique identifier for the resource.', 'dokan' ),
+                    'type'        => 'integer',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'date_created'          => array(
+                    'description' => __( "The date the variation was created, in the site's timezone.", 'dokan' ),
+                    'type'        => 'date-time',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'date_modified'         => array(
+                    'description' => __( "The date the variation was last modified, in the site's timezone.", 'dokan' ),
+                    'type'        => 'date-time',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'description'           => array(
+                    'description' => __( 'Variation description.', 'dokan' ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'permalink'             => array(
+                    'description' => __( 'Variation URL.', 'dokan' ),
+                    'type'        => 'string',
+                    'format'      => 'uri',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'sku'                   => array(
+                    'description' => __( 'Unique identifier.', 'dokan' ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'price'                 => array(
+                    'description' => __( 'Current variation price.', 'dokan' ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'regular_price'         => array(
+                    'description' => __( 'Variation regular price.', 'dokan' ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'sale_price'            => array(
+                    'description' => __( 'Variation sale price.', 'dokan' ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'date_on_sale_from'     => array(
+                    'description' => __( "Start date of sale price, in the site's timezone.", 'dokan' ),
+                    'type'        => 'date-time',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'date_on_sale_from_gmt' => array(
+                    'description' => __( 'Start date of sale price, as GMT.', 'dokan' ),
+                    'type'        => 'date-time',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'date_on_sale_to'       => array(
+                    'description' => __( "End date of sale price, in the site's timezone.", 'dokan' ),
+                    'type'        => 'date-time',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'date_on_sale_to_gmt'   => array(
+                    'description' => __( 'End date of sale price, as GMT.', 'dokan' ),
+                    'type'        => 'date-time',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'on_sale'               => array(
+                    'description' => __( 'Shows if the variation is on sale.', 'dokan' ),
+                    'type'        => 'boolean',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'visible'               => array(
+                    'description' => __( "Define if the variation is visible on the product's page.", 'dokan' ),
+                    'type'        => 'boolean',
+                    'default'     => true,
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'purchasable'           => array(
+                    'description' => __( 'Shows if the variation can be bought.', 'dokan' ),
+                    'type'        => 'boolean',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'virtual'               => array(
+                    'description' => __( 'If the variation is virtual.', 'dokan' ),
+                    'type'        => 'boolean',
+                    'default'     => false,
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'downloadable'          => array(
+                    'description' => __( 'If the variation is downloadable.', 'dokan' ),
+                    'type'        => 'boolean',
+                    'default'     => false,
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'downloads'             => array(
+                    'description' => __( 'List of downloadable files.', 'dokan' ),
+                    'type'        => 'array',
+                    'context'     => array( 'view', 'edit' ),
+                    'items'       => array(
+                        'type'       => 'object',
+                        'properties' => array(
+                            'id'   => array(
+                                'description' => __( 'File MD5 hash.', 'dokan' ),
+                                'type'        => 'string',
+                                'context'     => array( 'view', 'edit' ),
+                                'readonly'    => true,
+                            ),
+                            'name' => array(
+                                'description' => __( 'File name.', 'dokan' ),
+                                'type'        => 'string',
+                                'context'     => array( 'view', 'edit' ),
+                            ),
+                            'file' => array(
+                                'description' => __( 'File URL.', 'dokan' ),
+                                'type'        => 'string',
+                                'context'     => array( 'view', 'edit' ),
+                            ),
+                        ),
+                    ),
+                ),
+                'download_limit'        => array(
+                    'description' => __( 'Number of times downloadable files can be downloaded after purchase.', 'dokan' ),
+                    'type'        => 'integer',
+                    'default'     => -1,
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'download_expiry'       => array(
+                    'description' => __( 'Number of days until access to downloadable files expires.', 'dokan' ),
+                    'type'        => 'integer',
+                    'default'     => -1,
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'tax_status'            => array(
+                    'description' => __( 'Tax status.', 'dokan' ),
+                    'type'        => 'string',
+                    'default'     => 'taxable',
+                    'enum'        => array( 'taxable', 'shipping', 'none' ),
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'tax_class'             => array(
+                    'description' => __( 'Tax class.', 'dokan' ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'manage_stock'          => array(
+                    'description' => __( 'Stock management at variation level.', 'dokan' ),
+                    'type'        => 'mixed',
+                    'default'     => false,
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'stock_quantity'        => array(
+                    'description' => __( 'Stock quantity.', 'dokan' ),
+                    'type'        => 'integer',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'in_stock'              => array(
+                    'description' => __( 'Controls whether or not the variation is listed as "in stock" or "out of stock" on the frontend.', 'dokan' ),
+                    'type'        => 'boolean',
+                    'default'     => true,
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'backorders'            => array(
+                    'description' => __( 'If managing stock, this controls if backorders are allowed.', 'dokan' ),
+                    'type'        => 'string',
+                    'default'     => 'no',
+                    'enum'        => array( 'no', 'notify', 'yes' ),
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'backorders_allowed'    => array(
+                    'description' => __( 'Shows if backorders are allowed.', 'dokan' ),
+                    'type'        => 'boolean',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'backordered'           => array(
+                    'description' => __( 'Shows if the variation is on backordered.', 'dokan' ),
+                    'type'        => 'boolean',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'weight'                => array(
+                    /* translators: %s: weight unit */
+                    'description' => sprintf( __( 'Variation weight (%s).', 'dokan' ), $weight_unit ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'dimensions'            => array(
+                    'description' => __( 'Variation dimensions.', 'dokan' ),
+                    'type'        => 'object',
+                    'context'     => array( 'view', 'edit' ),
+                    'properties'  => array(
+                        'length' => array(
+                            /* translators: %s: dimension unit */
+                            'description' => sprintf( __( 'Variation length (%s).', 'dokan' ), $dimension_unit ),
+                            'type'        => 'string',
+                            'context'     => array( 'view', 'edit' ),
+                        ),
+                        'width'  => array(
+                            /* translators: %s: dimension unit */
+                            'description' => sprintf( __( 'Variation width (%s).', 'dokan' ), $dimension_unit ),
+                            'type'        => 'string',
+                            'context'     => array( 'view', 'edit' ),
+                        ),
+                        'height' => array(
+                            /* translators: %s: dimension unit */
+                            'description' => sprintf( __( 'Variation height (%s).', 'dokan' ), $dimension_unit ),
+                            'type'        => 'string',
+                            'context'     => array( 'view', 'edit' ),
+                        ),
+                    ),
+                ),
+                'shipping_class'        => array(
+                    'description' => __( 'Shipping class slug.', 'dokan' ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'shipping_class_id'     => array(
+                    'description' => __( 'Shipping class ID.', 'dokan' ),
+                    'type'        => 'string',
+                    'context'     => array( 'view', 'edit' ),
+                    'readonly'    => true,
+                ),
+                'image'                 => array(
+                    'description' => __( 'Variation image data.', 'dokan' ),
+                    'type'        => 'object',
+                    'context'     => array( 'view', 'edit' ),
+                    'properties'  => array(
+                        'id'                => array(
+                            'description' => __( 'Image ID.', 'dokan' ),
+                            'type'        => 'integer',
+                            'context'     => array( 'view', 'edit' ),
+                        ),
+                        'date_created'      => array(
+                            'description' => __( "The date the image was created, in the site's timezone.", 'dokan' ),
+                            'type'        => 'date-time',
+                            'context'     => array( 'view', 'edit' ),
+                            'readonly'    => true,
+                        ),
+                        'date_created_gmt'  => array(
+                            'description' => __( 'The date the image was created, as GMT.', 'dokan' ),
+                            'type'        => 'date-time',
+                            'context'     => array( 'view', 'edit' ),
+                            'readonly'    => true,
+                        ),
+                        'date_modified'     => array(
+                            'description' => __( "The date the image was last modified, in the site's timezone.", 'dokan' ),
+                            'type'        => 'date-time',
+                            'context'     => array( 'view', 'edit' ),
+                            'readonly'    => true,
+                        ),
+                        'date_modified_gmt' => array(
+                            'description' => __( 'The date the image was last modified, as GMT.', 'dokan' ),
+                            'type'        => 'date-time',
+                            'context'     => array( 'view', 'edit' ),
+                            'readonly'    => true,
+                        ),
+                        'src'               => array(
+                            'description' => __( 'Image URL.', 'dokan' ),
+                            'type'        => 'string',
+                            'format'      => 'uri',
+                            'context'     => array( 'view', 'edit' ),
+                        ),
+                        'name'              => array(
+                            'description' => __( 'Image name.', 'dokan' ),
+                            'type'        => 'string',
+                            'context'     => array( 'view', 'edit' ),
+                        ),
+                        'alt'               => array(
+                            'description' => __( 'Image alternative text.', 'dokan' ),
+                            'type'        => 'string',
+                            'context'     => array( 'view', 'edit' ),
+                        ),
+                        'position'          => array(
+                            'description' => __( 'Image position. 0 means that the image is featured.', 'dokan' ),
+                            'type'        => 'integer',
+                            'context'     => array( 'view', 'edit' ),
+                        ),
+                    ),
+                ),
+                'attributes'            => array(
+                    'description' => __( 'List of attributes.', 'dokan' ),
+                    'type'        => 'array',
+                    'context'     => array( 'view', 'edit' ),
+                    'items'       => array(
+                        'type'       => 'object',
+                        'properties' => array(
+                            'id'     => array(
+                                'description' => __( 'Attribute ID.', 'dokan' ),
+                                'type'        => 'integer',
+                                'context'     => array( 'view', 'edit' ),
+                            ),
+                            'name'   => array(
+                                'description' => __( 'Attribute name.', 'dokan' ),
+                                'type'        => 'string',
+                                'context'     => array( 'view', 'edit' ),
+                            ),
+                            'option' => array(
+                                'description' => __( 'Selected attribute term name.', 'dokan' ),
+                                'type'        => 'string',
+                                'context'     => array( 'view', 'edit' ),
+                            ),
+                        ),
+                    ),
+                ),
+                'menu_order'            => array(
+                    'description' => __( 'Menu order, used to custom sort products.', 'dokan' ),
+                    'type'        => 'integer',
+                    'context'     => array( 'view', 'edit' ),
+                ),
+                'meta_data'             => array(
+                    'description' => __( 'Meta data.', 'dokan' ),
+                    'type'        => 'array',
+                    'context'     => array( 'view', 'edit' ),
+                    'items'       => array(
+                        'type'       => 'object',
+                        'properties' => array(
+                            'id'    => array(
+                                'description' => __( 'Meta ID.', 'dokan' ),
+                                'type'        => 'integer',
+                                'context'     => array( 'view', 'edit' ),
+                                'readonly'    => true,
+                            ),
+                            'key'   => array(
+                                'description' => __( 'Meta key.', 'dokan' ),
+                                'type'        => 'string',
+                                'context'     => array( 'view', 'edit' ),
+                            ),
+                            'value' => array(
+                                'description' => __( 'Meta value.', 'dokan' ),
+                                'type'        => 'mixed',
+                                'context'     => array( 'view', 'edit' ),
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        );
 
+        return $this->add_additional_fields_schema( $schema );
+    }
 }
