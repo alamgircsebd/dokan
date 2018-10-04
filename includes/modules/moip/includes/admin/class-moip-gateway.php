696957eb7345ab319fa65c83855eb6e640b1aa86
saimonh3 <saimon2721@gmail.com>
2018-10-04 12:35:08 +0600

    fix: use correct function to get seller id by an order

--- includes/modules/moip/includes/admin/class-moip-gateway.php
+++ includes/modules/moip/includes/admin/class-moip-gateway.php
@@ -478,7 +478,7 @@ class Dokan_Moip_Connect extends WC_Payment_Gateway {
 
         foreach ( $all_orders as $tmp_order ) {
             $tmp_order_id = dokan_get_prop( $tmp_order, 'id' );
-            $seller_id    = get_post_field( 'post_author', $tmp_order_id );
+            $seller_id    = dokan_get_seller_id_by_order( $tmp_order_id );
             $do_order     = $this->get_dokan_order( $tmp_order_id, $seller_id );
 
             // in-case we can't find the order
