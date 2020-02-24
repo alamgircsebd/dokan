25dbff340104db53fa8a86194d28f4bc0de55906
Mohammed Saimon <saimon2721@gmail.com>
2020-02-24 05:38:06 +0000

    Merged in refactor/store-support (pull request #522)
    fix: make store support template overridable

--- modules/store-support/templates/store-support/support-tickets.php
+++ modules/store-support/templates/store-support/support-tickets.php
@@ -1,24 +1,25 @@
 <?php
+/**
+ * My Account page
+ *
+ * This template can be overridden by copying it to yourtheme/dokan/store-support/support-tickets.php.
+ *
+ * @package Dokan/Templates
+ */
 
-do_action( 'woocommerce_account_navigation' ); ?>
+defined( 'ABSPATH' ) || exit;
 
-<div class="woocommerce-MyAccount-content">
-	<?php
-		$dss      = dokan_pro()->module->store_support;
-		$topic_id = get_query_var( 'support-tickets' );
+$dss      = dokan_pro()->module->store_support;
+$topic_id = get_query_var( 'support-tickets' );
+$topic    = is_numeric( $topic_id ) ? $dss->get_single_topic_by_customer( $topic_id, dokan_get_current_user_id() ) : ''; ?>
 
-		if( is_numeric( $topic_id ) ) {
-		    $topic = $dss->get_single_topic_by_customer( $topic_id, dokan_get_current_user_id() );
-		}
-	?>
-	<div class="dokan-support-customer-listing dokan-support-topic-wrapper">
-	    <?php
-	        if ( empty( $topic ) || isset( $_GET['ticket_status'] ) ) {
-	            $dss->support_topic_status_list( false );
-	            $dss->print_support_topics_by_customer( dokan_get_current_user_id() );
-	        } else {
-	            $dss->print_single_topic( $topic );
-	        }
-	    ?>
-	</div>
+<div class="dokan-support-customer-listing dokan-support-topic-wrapper">
+    <?php
+        if ( empty( $topic ) || isset( $_GET['ticket_status'] ) ) {
+            $dss->support_topic_status_list( false );
+            $dss->print_support_topics_by_customer( dokan_get_current_user_id() );
+        } else {
+            $dss->print_single_topic( $topic );
+        }
+    ?>
 </div>
