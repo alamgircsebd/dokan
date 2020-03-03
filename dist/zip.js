82e6beccb3d9a999b5a8e14a50c99ae30b0f2644
Edi Amin <to.ediamin@gmail.com>
2020-03-03 12:10:34 +0600

    Add script to upload built zips on BitBucket

--- dist/zip.js
+++ dist/zip.js
@@ -1,5 +1,4 @@
 const fs = require( 'fs-extra' );
-const path = require( 'path' );
 const { exec } = require( 'child_process' );
 const util = require( 'util' );
 const replace = require( 'replace-in-file' );
