efe3c69670fbe5721028c6bab02afb9a4c4b53d9
Kapil Paul <kapilpaul007@gmail.com>
2020-11-30 16:37:01 +0600

    refactor: empty instead of blank string check

--- includes/REST/LogsController.php
+++ includes/REST/LogsController.php
@@ -121,7 +121,7 @@ class LogsController extends DokanRESTAdminController {
 
             $gateway_fee_paid_by = $order->get_meta( 'dokan_gateway_fee_paid_by', true );
 
-            if ( $processing_fee && '' === $gateway_fee_paid_by ) {
+            if ( $processing_fee && empty( $gateway_fee_paid_by ) ) {
                 $gateway_fee_paid_by = 'admin';
             }
 
