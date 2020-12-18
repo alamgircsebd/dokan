956c8721a91fc4b1a93c85e9b9d7ac2fb9acd640
Zabir Ahmed Anik <xaviranik.me@gmail.com>
2020-12-18 23:20:23 +0600

    fix: page wrapper issue for vendor bio, store-review page (#907)

--- templates/vendor-biography.php
+++ templates/vendor-biography.php
@@ -8,13 +8,18 @@
 $store_user = get_userdata( get_query_var( 'author' ) );
 $store_info   = dokan_get_store_info( $store_user->ID );
 $map_location = $store_user->get_location();
+$layout       = get_theme_mod( 'store_layout', 'left' );
 
 get_header( 'shop' );
 ?>
 
 <?php do_action( 'woocommerce_before_main_content' ); ?>
 
-<?php dokan_get_template_part( 'store', 'sidebar', array( 'store_user' => $store_user, 'store_info' => $store_info, 'map_location' => $map_location ) ); ?>
+<div class="dokan-store-wrap layout-<?php echo esc_attr( $layout ); ?>">
+
+    <?php if ( 'left' === $layout ) { ?>
+        <?php dokan_get_template_part( 'store', 'sidebar', array( 'store_user' => $store_user, 'store_info' => $store_info, 'map_location' => $map_location ) ); ?>
+    <?php } ?>
 
 <div id="dokan-primary" class="dokan-single-store dokan-w8">
     <div id="dokan-content" class="store-review-wrap woocommerce" role="main">
@@ -39,6 +44,12 @@ get_header( 'shop' );
 
     </div><!-- #content .site-content -->
 </div><!-- #primary .content-area -->
+
+    <?php if ( 'right' === $layout ) { ?>
+        <?php dokan_get_template_part( 'store', 'sidebar', array( 'store_user' => $store_user, 'store_info' => $store_info, 'map_location' => $map_location ) ); ?>
+    <?php } ?>
+
+</div><!-- .dokan-store-wrap -->
 
 <?php do_action( 'woocommerce_after_main_content' ); ?>
 
