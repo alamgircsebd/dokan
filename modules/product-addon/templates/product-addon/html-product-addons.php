5474df6b184e1bb8c48c3c102947d8bc3bf43ded
Alamgir Hossain <alamgircse.bd@gmail.com>
2021-01-07 14:39:22 +0600

    update: Product add-on module template override does not work issue fixed #785 (#925)

--- modules/product-addon/templates/product-addon/html-product-addons.php
+++ modules/product-addon/templates/product-addon/html-product-addons.php
@@ -8,8 +8,8 @@ $product_addons = array_filter( (array) $product->get_meta( '_product_addons' ) 
 
 <div id="dokan-product-addons-options" class="dokan-product-addons-options dokan-edit-row dokan-clearfix">
     <div class="dokan-section-heading" data-togglehandler="dokan_product_addons_options">
-        <h2><i class="fa fa-wrench" aria-hidden="true"></i> <?php _e( 'Add-ons', 'dokan' ); ?><span class=""></h2>
-        <p class=""><?php _e( 'Manage addon fields for this product.', 'dokan' ); ?></p>
+        <h2><i class="fa fa-wrench" aria-hidden="true"></i> <?php esc_html_e( 'Add-ons', 'dokan' ); ?><span class=""></h2>
+        <p class=""><?php esc_html_e( 'Manage addon fields for this product.', 'dokan' ); ?></p>
 
         <a href="#" class="dokan-section-toggle">
             <i class="fa fa-sort-desc fa-flip-vertical" aria-hidden="true"></i>
@@ -17,6 +17,14 @@ $product_addons = array_filter( (array) $product->get_meta( '_product_addons' ) 
         <div class="dokan-clearfix"></div>
     </div>
     <div class="dokan-section-content">
-        <?php include( dirname( __FILE__ ) . '/html-addon-panel.php' ); ?>
+        <?php
+        dokan_get_template_part(
+            'product-addon/html-addon-panel', '', array(
+				'is_product_addon' => true,
+				'exists'           => $exists,
+				'product_addons'   => $product_addons,
+            )
+        );
+        ?>
     </div>
 </div>
